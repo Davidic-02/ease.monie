245aa55026fa70baa998babe7cbe1403cbbf10c5
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-08 19:18:55 +0100

    chore: remove unused imports in BankToBank widget

--- lib/presentation/screens/home/quick_actions/bank_to_bank/bank_to_bank.dart
+++ lib/presentation/screens/home/quick_actions/bank_to_bank/bank_to_bank.dart
@@ -1,7 +1,5 @@
 import 'package:esae_monie/constants/app_spacing.dart';
 import 'package:esae_monie/extensions/build_context.dart';
-import 'package:esae_monie/presentation/data/lists.dart';
-import 'package:esae_monie/presentation/widgets/custom_horizontal_scroll.dart';
 import 'package:esae_monie/presentation/widgets/custom_topbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
