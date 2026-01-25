6f13df2f5f8a7a87bd4dadf4013a3b3a7b505aa9
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-25 20:47:56 +0100

    Remove unused insurance screen import from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:esae_monie/constants/theme_data.dart';
 import 'package:esae_monie/presentation/data/lists.dart';
 import 'package:esae_monie/presentation/screens/auth/sign_in.dart';
 import 'package:esae_monie/presentation/screens/home/services/gift/gift.dart';
-import 'package:esae_monie/presentation/screens/home/services/insurance/insurance.dart';
 import 'package:esae_monie/router/app_routes.dart';
 import 'package:esae_monie/services/service_locator.dart';
 import 'package:esae_monie/services/theme_services.dart';
