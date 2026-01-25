19b7c11137f410fd7f835a8a7e9d72fcd823cfeb
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-25 20:32:34 +0100

    Refactor Gift Bloc and Update Gift Model: Replace _Started event with _Init, enhance GiftState with giftModel, and implement ServiceCards for displaying gifts with cashback options.

--- lib/presentation/widgets/services_card.dart
+++ lib/presentation/widgets/services_card.dart
@@ -1,6 +1,8 @@
 import 'package:esae_monie/constants/app_colors.dart';
 import 'package:esae_monie/constants/app_spacing.dart';
+import 'package:esae_monie/extensions/build_context.dart';
 import 'package:esae_monie/models/services_model.dart';
+import 'package:esae_monie/presentation/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
@@ -42,7 +44,7 @@ class ServiceCard extends StatelessWidget {
     if (daysLeft == null) return Colors.green;
 
     if (daysLeft! == 0) {
-      return Colors.red.shade600; // Expires today
+      return Colors.red.shade600;
     } else if (daysLeft! <= 2) {
       return Colors.red.shade400;
     } else if (daysLeft! <= 5) {
@@ -257,6 +259,119 @@ class ServiceCard extends StatelessWidget {
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ServiceCards extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String subTitle;
+  final String? cashBack;
+  final Function()? onPressed;
+
+  const ServiceCards({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.subTitle,
+    this.cashBack,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppSpacing.horizontalSpacing),
+      child: Material(
+        elevation: 1,
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                ),
+                child: Image.asset(
+                  imagePath,
+                  width: double.infinity,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              AppSpacing.verticalSpaceMedium,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppSpacing.horizontalSpacing,
+                ),
+                child: Text(
+                  title,
+                  style: context.textTheme.bodyLarge?.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppSpacing.horizontalSpacing,
+                ),
+                child: Text(
+                  subTitle,
+                  style: context.textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(
+                      context,
+                    ).colorScheme.onSurface.withValues(alpha: .4),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppSpacing.horizontalSpacing,
+                  vertical: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 7,
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Get ',
+                          style: context.textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.w900,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: cashBack,
+                              style: context.textTheme.bodyMedium?.copyWith(
+                                fontWeight: FontWeight.w900,
+                                color: AppColors.blueColor,
+                              ),
+                            ),
+
+                            TextSpan(text: ' Cashback'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Button('Send', onPressed: onPressed),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
