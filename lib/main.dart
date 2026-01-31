e339b96d4824c26fd7ee1f33024db7f6b937413f
David Adekoya <davidadekoya88@gmail.com>
2026-01-31 11:58:44 +0100

    adjusted the main

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:esae_monie/constants/theme_data.dart';
 import 'package:esae_monie/presentation/data/lists.dart';
 import 'package:esae_monie/presentation/screens/auth/sign_in.dart';
 import 'package:esae_monie/presentation/screens/home/services/gift/gift.dart';
+import 'package:esae_monie/presentation/screens/home/services/insurance/insurance.dart';
 import 'package:esae_monie/router/app_routes.dart';
 import 'package:esae_monie/services/service_locator.dart';
 import 'package:esae_monie/services/theme_services.dart';
@@ -56,11 +57,7 @@ class MyApp extends StatelessWidget {
               CharityBloc()..add(CharityEvent.started([charity1, charity2])),
         ),
         BlocProvider<GiftBloc>(create: (_) => GiftBloc()),
-        BlocProvider<InsuranceBloc>(
-          create: (_) => InsuranceBloc()
-            ..add(InsuranceEvent.started([insurance1, insurance2, insurance3])),
-          child: Gift(),
-        ),
+        BlocProvider<InsuranceBloc>(create: (_) => InsuranceBloc()),
       ],
       child: ValueListenableBuilder<ThemeMode>(
         valueListenable: ThemeService.themeModeNotifier,
