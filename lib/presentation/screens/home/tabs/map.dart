f53b9819ac1a7e48bfa70a6aa7fd4b41a9cfef73
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-02-08 20:47:51 +0100

    feat: add MapScreen and update import path for Home screen

---
+++ lib/presentation/screens/home/tabs/map.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  static const String routeName = 'map_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
